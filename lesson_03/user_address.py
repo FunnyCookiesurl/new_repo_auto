21e49226d21da817a73eb25686669b36967ba567
FunnyCookiesurl <sonjinwoo70@gmail.com>
2025-09-28 20:07:35 +0300

    Добавил ДЗ по pytest: класс StringUtils и тесты

--- lesson_03/user_address.py
+++
@@ -1,7 +0,0 @@
-class Address:
-    def __init__(self, index, city, street, home, apartment):
-        self.index = index
-        self.city = city
-        self.street = street
-        self.home = home
-        self.apartment = apartment
