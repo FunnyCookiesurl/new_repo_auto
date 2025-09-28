1e9083cceceaf3a6220b042b6ff53c8029bfba7f
FunnyCookiesurl <sonjinwoo70@gmail.com>
2025-09-28 20:04:52 +0300

    Добавил lesson 4 - pytest

--- lesson_04/test_string_utils.py
+++ lesson_04/test_string_utils.py
@@ -3,6 +3,7 @@ from string_utils import StringUtils
 
 string_utils = StringUtils()
 
+
 @pytest.mark.parametrize(
     "input_text, expected_output",
     [
@@ -11,11 +12,10 @@ string_utils = StringUtils()
         ("a", "A"),
         ("A", "A"),
         ("123abc", "123abc"),
-        (" skypro", " skypro") 
-    ]
-)
+        (" skypro", " skypro")])
 def test_capitalize(input_text, expected_output):
     assert string_utils.capitalize(input_text) == expected_output
+
 
 @pytest.mark.parametrize(
     "input_text, expected_output",
@@ -24,11 +24,10 @@ def test_capitalize(input_text, expected_output):
         ("", ""),
         ("   ", ""),
         ("skypro", "skypro"),
-        (" skypro", "skypro")
-    ]
-)
+        (" skypro", "skypro")])
 def test_trim(input_text, expected_output):
     assert string_utils.trim(input_text) == expected_output
+
 
 @pytest.mark.parametrize(
     "string, symbol, expected",
@@ -38,12 +37,10 @@ def test_trim(input_text, expected_output):
         ("", "a", False),
         ("Sky Pro", " ", True),
         ("Sky123", "1", True),
-        ("SkyPro", "", True)
-    ]
-)
-
+        ("SkyPro", "", True)])
 def test_contains(string, symbol, expected):
     assert string_utils.contains(string, symbol) == expected
+
 
 @pytest.mark.parametrize(
     "string, symbol, expected",
@@ -52,9 +49,6 @@ def test_contains(string, symbol, expected):
         ("SkyPro", "Pro", "Sky"),
         ("", "a", ""),
         ("SkyPro", "x", "SkyPro"),
-        ("Sky123", "1", "Sky23")
-    ]
-)
-
+        ("Sky123", "1", "Sky23")])
 def test_delete_symbol(string, symbol, expected):
     assert string_utils.delete_symbol(string, symbol) == expected
